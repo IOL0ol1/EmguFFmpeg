6493fc4a0337cbdd85fb5c58cd1776c9e5e761f0
IOL0ol1 <1353839731@qq.com>
2019-10-24 23:38:44 +0800

    add example

---
+++ EmguFFmpeg.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmguFFmpeg.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
