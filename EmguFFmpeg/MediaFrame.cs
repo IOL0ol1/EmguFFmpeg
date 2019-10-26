a933e1331e08683d054344ca5fb2d8ef9a736432
IOL0ol1 <1353839731@qq.com>
2019-10-26 21:44:53 +0800

    add some useful function

--- EmguFFmpeg/MediaFrame.cs
+++ EmguFFmpeg/MediaFrame.cs
@@ -19,7 +19,7 @@ namespace EmguFFmpeg
         public AVFrame Frame => *pFrame;
 
         /// <summary>
-        /// Deep clone frame(include data)
+        /// Deep clone frame
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
@@ -45,22 +45,47 @@ namespace EmguFFmpeg
             return Clone<MediaFrame>();
         }
 
-        public byte[][] GetManagedData()
+        /// <summary>
+        /// Get managed copy of <see cref="AVFrame.data"/> by <see cref="AVFrame.linesize"/>
+        /// <para>
+        /// NOTE: length maybe greater than valid data, because memory alignment.
+        /// </para>
+        /// </summary>
+        /// <returns></returns>
+        public byte[][] ToArray()
         {
             List<byte[]> result = new List<byte[]>();
             IntPtr intPtr;
-            for (uint i = 0; (intPtr = (IntPtr)pFrame->data[i]) != IntPtr.Zero; i++)
+            for (uint i = 0; (intPtr = (IntPtr)pFrame->extended_data[i]) != IntPtr.Zero; i++)
             {
-                byte[] line = new byte[pFrame->linesize[i]];
+                if (0 < pFrame->channels && pFrame->channels < i)
+                    break;
+                byte[] line = new byte[pFrame->linesize[pFrame->channels > 0 ? 0 : i]];
                 Marshal.Copy(intPtr, line, 0, line.Length);
                 result.Add(line);
             }
             return result.ToArray();
         }
 
-        public byte*[] Data
+        public void Clear()
         {
-            get => pFrame->data;
+            ffmpeg.av_frame_unref(pFrame);
+        }
+
+        public IntPtr[] Data
+        {
+            get
+            {
+                List<IntPtr> result = new List<IntPtr>();
+                IntPtr intPtr;
+                for (uint i = 0; (intPtr = (IntPtr)pFrame->extended_data[i]) != IntPtr.Zero; i++)
+                {
+                    if (0 < pFrame->channels && pFrame->channels < i)
+                        break;
+                    result.Add(intPtr);
+                }
+                return result.ToArray();
+            }
         }
 
         public int[] Linesize
@@ -187,12 +212,35 @@ namespace EmguFFmpeg
 
         #region IDisposable Support
 
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                fixed (AVFrame** ppFrame = &pFrame)
+                {
+                    ffmpeg.av_frame_free(ppFrame);
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        ~MediaFrame()
+        {
+            Dispose(false);
+        }
+
         public void Dispose()
         {
-            fixed (AVFrame** ppFrame = &pFrame)
-            {
-                ffmpeg.av_frame_free(ppFrame);
-            }
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         #endregion
@@ -205,10 +253,21 @@ namespace EmguFFmpeg
 
         public VideoFrame(AVPixelFormat format, int width, int height, int align = 0) : base()
         {
+            AllocBuffer(format, width, height, align);
+        }
+
+        private void AllocBuffer(AVPixelFormat format, int width, int height, int align = 0)
+        {
             pFrame->format = (int)format;
             pFrame->width = width;
             pFrame->height = height;
             ffmpeg.av_frame_get_buffer(pFrame, align);
+        }
+
+        public void Init(AVPixelFormat format, int width, int height, int align = 0)
+        {
+            Clear();
+            AllocBuffer(format, width, height, align);
         }
 
 #if NETFRAMEWORK
@@ -259,6 +318,16 @@ namespace EmguFFmpeg
         /// </param>
         public AudioFrame(AVSampleFormat format, ulong channelLayout, int nbSamples, int sampleRate = 0, int align = 0) : base()
         {
+            AllocBuffer(format, channelLayout, nbSamples, sampleRate, align);
+        }
+
+        public AudioFrame(AVSampleFormat format, int channels, int nbSamples, int sampleRate = 0, int align = 0) : base()
+        {
+            AllocBuffer(format, (ulong)ffmpeg.av_get_default_channel_layout(channels), nbSamples, sampleRate, align);
+        }
+
+        private void AllocBuffer(AVSampleFormat format, ulong channelLayout, int nbSamples, int sampleRate = 0, int align = 0)
+        {
             pFrame->format = (int)format;
             pFrame->channel_layout = channelLayout;
             pFrame->nb_samples = nbSamples;
@@ -266,13 +335,16 @@ namespace EmguFFmpeg
             ffmpeg.av_frame_get_buffer(pFrame, align);
         }
 
-        public AudioFrame(AVSampleFormat format, int channels, int nbSamples, int sampleRate = 0, int align = 0) : base()
+        public void Init(AVSampleFormat format, int channels, int nbSamples, int sampleRate = 0, int align = 0)
         {
-            pFrame->format = (int)format;
-            pFrame->channel_layout = (ulong)ffmpeg.av_get_default_channel_layout(channels);
-            pFrame->nb_samples = nbSamples;
-            pFrame->sample_rate = sampleRate;
-            ffmpeg.av_frame_get_buffer(pFrame, align);
+            Clear();
+            AllocBuffer(format, (ulong)ffmpeg.av_get_default_channel_layout(channels), nbSamples, sampleRate, align);
+        }
+
+        public void Init(AVSampleFormat format, ulong channelLayout, int nbSamples, int sampleRate = 0, int align = 0)
+        {
+            Clear();
+            AllocBuffer(format, channelLayout, nbSamples, sampleRate, align);
         }
 
         public byte[][] ToSamples()
