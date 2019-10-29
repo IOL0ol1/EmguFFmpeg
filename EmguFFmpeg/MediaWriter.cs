0a9fcd798e6106b099dea0a227c0d462313934bc
IOL0ol1 <1353839731@qq.com>
2019-10-29 18:16:28 +0800

    add comment

--- EmguFFmpeg/MediaWriter.cs
+++ EmguFFmpeg/MediaWriter.cs
@@ -9,6 +9,13 @@ namespace EmguFFmpeg
     {
         public new OutFormat Format => base.Format as OutFormat;
 
+        /// <summary>
+        /// <para><see cref="ffmpeg.avformat_alloc_output_context2(AVFormatContext**, AVOutputFormat*, string, string)"/></para>
+        /// <para><see cref="ffmpeg.avio_open(AVIOContext**, string, int)"/></para>
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="oformat"></param>
+        /// <param name="options"></param>
         public MediaWriter(string file, OutFormat oformat = null, MediaDictionary options = null)
         {
             fixed (AVFormatContext** ppFormatContext = &pFormatContext)
@@ -20,6 +27,9 @@ namespace EmguFFmpeg
                 ffmpeg.avio_open2(&pFormatContext->pb, file, ffmpeg.AVIO_FLAG_WRITE, null, options).ThrowExceptionIfError();
         }
 
+        /// <summary>
+        /// <see cref=" ffmpeg.av_dump_format(AVFormatContext*, int, string, int)"/>
+        /// </summary>
         public override void DumpInfo()
         {
             for (int i = 0; i < pFormatContext->nb_streams; i++)
@@ -28,22 +38,49 @@ namespace EmguFFmpeg
             }
         }
 
+        /// <summary>
+        /// Add stream by encode
+        /// <para><see cref="ffmpeg.avformat_new_stream(AVFormatContext*, AVCodec*)"/></para>
+        /// <para>set <see cref="AVStream.id"/></para>
+        /// <para><see cref="ffmpeg.avcodec_parameters_from_context(AVCodecParameters*, AVCodecContext*)"/></para>
+        /// <para>set <see cref="AVStream.time_base"/> from <see cref="AVCodecContext.time_base"/></para>
+        /// </summary>
+        /// <param name="encode">Used to codec stream.
+        /// <para>
+        /// set null to add a data stream but no encoder,
+        /// then use <see cref="WritePacket(MediaPacket)"/> write data directly.
+        /// </para>
+        /// </param>
+        /// <returns></returns>
         public MediaStream AddStream(MediaEncode encode)
         {
             AVStream* stream = ffmpeg.avformat_new_stream(pFormatContext, null);
             stream->id = (int)(pFormatContext->nb_streams - 1);
             // keep the order
-            ffmpeg.avcodec_parameters_from_context(stream->codecpar, encode);
-            stream->time_base = encode.AVCodecContext.time_base;
+            if (encode != null)
+            {
+                ffmpeg.avcodec_parameters_from_context(stream->codecpar, encode);
+                stream->time_base = encode.AVCodecContext.time_base;
+            }
             streams.Add(new MediaStream(stream) { Codec = encode });
             return streams.Last();
         }
 
+        /// <summary>
+        /// <see cref="ffmpeg.avformat_write_header(AVFormatContext*, AVDictionary**)"/>
+        /// </summary>
+        /// <param name="options"></param>
         public void Initialize(MediaDictionary options = null)
         {
             ffmpeg.avformat_write_header(pFormatContext, options).ThrowExceptionIfError();
         }
 
+        /// <summary>
+        /// <para><see cref="ffmpeg.av_interleaved_write_frame(AVFormatContext*, AVPacket*)"/></para>
+        /// <para><see cref="ffmpeg.av_packet_unref"/></para>
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <returns></returns>
         public int WritePacket(MediaPacket packet)
         {
             int ret = ffmpeg.av_interleaved_write_frame(pFormatContext, packet);
@@ -51,6 +88,11 @@ namespace EmguFFmpeg
             return ret;
         }
 
+        /// <summary>
+        /// send null frame and receive packet to flush encoder cache.
+        /// <para><see cref="MediaStream.WriteFrame(MediaFrame)"/></para>
+        /// <para><see cref="WritePacket(MediaPacket)"/></para>
+        /// </summary>
         public void FlushMuxer()
         {
             foreach (var stream in streams)
@@ -69,6 +111,12 @@ namespace EmguFFmpeg
 
         #region IDisposable
 
+        /// <summary>
+        /// <para><see cref="ffmpeg.av_write_trailer(AVFormatContext*)"/></para>
+        /// <para><see cref="ffmpeg.avio_close(AVIOContext*)"/></para>
+        /// <para><see cref="ffmpeg.avformat_free_context(AVFormatContext*)"/></para>
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (pFormatContext != null)
