3a9290af7e5e94752eac53b0f662a1b9126870f9
IOL0ol1 <1353839731@qq.com>
2019-11-08 01:50:53 +0800

    add a frame to emgucv image example

---
+++ EmguFFmpeg.Example/DecodeVideoToImage.cs
@@ -0,0 +1,39 @@
+﻿using FFmpeg.AutoGen;
+using EmguFFmpeg.EmguCV;
+using System.IO;
+using System.Linq;
+using Emgu.CV;
+using System;
+
+namespace EmguFFmpeg.Example
+{
+    class DecodeVideoToImage
+    {
+        /// <summary>
+        /// decode video to image
+        /// </summary>
+        /// <param name="inputFile">input video file</param>
+        /// <param name="outDirectory">folder for output image files</param>
+        public DecodeVideoToImage(string inputFile, string outDirectory)
+        {
+            string outputdir = Directory.CreateDirectory(outDirectory).FullName;
+            using (MediaReader reader = new MediaReader(inputFile))
+            {
+                var videoIndex = reader.Where(_ => _.Codec.AVCodecContext.codec_type == AVMediaType.AVMEDIA_TYPE_VIDEO).First().Index;
+
+                foreach (var packet in reader.ReadPacket())
+                {
+                    foreach (var frame in reader[videoIndex].ReadFrame(packet))
+                    {
+                        using (var image = frame.ToImage())
+                        {
+                            // convert pts to timespan string
+                            string ts = reader[videoIndex].ToTimeSpan(frame.Pts).ToString().Replace(":", ".");
+                            image.Save(Path.Combine(outputdir, $"{ts}.bmp"));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
