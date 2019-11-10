de4493bed146a549d17a954d45dd9e5be98dd5e0
IOL0ol1 <1353839731@qq.com>
2019-11-11 01:35:15 +0800

    use avframe func

--- EmguFFmpeg.EmguCV/EmguFFmpegExtension.cs
+++ EmguFFmpeg.EmguCV/EmguFFmpegExtension.cs
@@ -30,66 +30,46 @@ namespace EmguFFmpeg.EmguCV
                 return VideoFrameToMat(frame);
             else if (frame.IsAudioFrame)
                 return AudioFrameToMat(frame);
-            throw new FFmpegException(FFmpegException.ErrorMessages.InvalidFrame);
+            throw new FFmpegException(FFmpegException.InvalidFrame);
         }
 
         private static Mat AudioFrameToMat(MediaFrame frame)
         {
-            switch ((AVSampleFormat)frame.Format)
+            frame = (frame as AudioFrame).ToPlanar();
+            switch ((AVSampleFormat)frame.AVFrame.format)
             {
                 case AVSampleFormat.AV_SAMPLE_FMT_U8:
-                    return PlanarToMat<byte>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_U8P));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_U8P:
                     return PlanarToMat<byte>(frame);
 
                 case AVSampleFormat.AV_SAMPLE_FMT_S16:
-                    return PlanarToMat<short>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_S16P));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_S16P:
                     return PlanarToMat<short>(frame);
 
                 case AVSampleFormat.AV_SAMPLE_FMT_S32:
-                    return PlanarToMat<int>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_S32P));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_S32P:
                     return PlanarToMat<int>(frame);
 
                 case AVSampleFormat.AV_SAMPLE_FMT_FLT:
-                    return PlanarToMat<float>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_FLTP));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_FLTP:
                     return PlanarToMat<float>(frame);
 
                 case AVSampleFormat.AV_SAMPLE_FMT_DBL:
-                    return PlanarToMat<double>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_DBLP));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_DBLP:
                     return PlanarToMat<double>(frame);
 
                 case AVSampleFormat.AV_SAMPLE_FMT_S64:
-                    return PlanarToMat<long>(PacketToPlanar(frame, AVSampleFormat.AV_SAMPLE_FMT_S64P));
-
                 case AVSampleFormat.AV_SAMPLE_FMT_S64P:
                     return PlanarToMat<long>(frame);
 
                 default:
-                    throw new FFmpegException(FFmpegException.ErrorMessages.NotSupportFormat);
+                    throw new FFmpegException(FFmpegException.NotSupportFormat);
             }
         }
 
-        public static MediaFrame PacketToPlanar(MediaFrame frame, AVSampleFormat dstPlanarFormat)
+        private static Mat PlanarToMat<T>(MediaFrame frame) where T : struct
         {
-            AudioFrame dstFrame = new AudioFrame(dstPlanarFormat, frame.ChannelLayout, frame.NbSamples, frame.SampleRate);
-            using (SampleConverter converter = new SampleConverter(dstFrame))
-            {
-                return converter.ConvertFrame(frame, out int a, out int b);
-            }
-        }
-
-        private static Mat PlanarToMat<T>(MediaFrame frame) where T : new()
-        {
-            Image<Gray, T> image = new Image<Gray, T>(frame.NbSamples, frame.Channels);
+            Image<Gray, T> image = new Image<Gray, T>(frame.AVFrame.nb_samples, frame.AVFrame.channels);
             int stride = image.MIplImage.WidthStep;
             byte[][] planes = frame.GetData();
             for (int i = 0; i < frame.Channels; i++)
@@ -101,9 +81,9 @@ namespace EmguFFmpeg.EmguCV
 
         private static Mat VideoFrameToMat(MediaFrame frame)
         {
-            if ((AVPixelFormat)frame.Format != AVPixelFormat.AV_PIX_FMT_BGR24)
+            if ((AVPixelFormat)frame.AVFrame.format != AVPixelFormat.AV_PIX_FMT_BGR24)
             {
-                using (VideoFrame dstFrame = new VideoFrame(AVPixelFormat.AV_PIX_FMT_BGR24, frame.Width, frame.Height))
+                using (VideoFrame dstFrame = new VideoFrame(AVPixelFormat.AV_PIX_FMT_BGR24, frame.AVFrame.width, frame.AVFrame.height))
                 using (PixelConverter converter = new PixelConverter(dstFrame))
                 {
                     return Bgr24ToMat(converter.ConvertFrame(frame));
@@ -114,12 +94,12 @@ namespace EmguFFmpeg.EmguCV
 
         private static Mat Bgr24ToMat(MediaFrame frame)
         {
-            Mat mat = new Mat(frame.Height, frame.Width, DepthType.Cv8U, 3);
+            Mat mat = new Mat(frame.AVFrame.height, frame.AVFrame.width, DepthType.Cv8U, 3);
             int stride = mat.Step;
             byte[] plane = frame.GetData()[0];
-            for (int i = 0; i < frame.Height; i++)
+            for (int i = 0; i < frame.AVFrame.height; i++)
             {
-                Marshal.Copy(plane, i * frame.Linesize[0], IntPtr.Add(mat.DataPointer, i * stride), stride);
+                Marshal.Copy(plane, i * frame.AVFrame.linesize[0], IntPtr.Add(mat.DataPointer, i * stride), stride);
             }
             return mat;
         }
@@ -183,7 +163,7 @@ namespace EmguFFmpeg.EmguCV
                     break;
 
                 default:
-                    throw new FFmpegException(FFmpegException.ErrorMessages.NotSupportFormat);
+                    throw new FFmpegException(FFmpegException.NotSupportFormat);
             }
 
             if (dstFotmat != AVSampleFormat.AV_SAMPLE_FMT_S16P)
@@ -216,7 +196,7 @@ namespace EmguFFmpeg.EmguCV
                 else if (typeof(T) == typeof(double))
                     format = AVSampleFormat.AV_SAMPLE_FMT_DBLP;
                 else
-                    throw new FFmpegException(FFmpegException.ErrorMessages.NotSupportFormat);
+                    throw new FFmpegException(FFmpegException.NotSupportFormat);
                 AudioFrame frame = new AudioFrame(format, image.Height, image.Width, dstSampleRate);
                 int stride = image.Width * image.NumberOfChannels;
                 for (int i = 0; i < image.Height; i++)
@@ -246,15 +226,15 @@ namespace EmguFFmpeg.EmguCV
             }
         }
 
-        private static VideoFrame MatToVideoFrame(Mat mat)
+        private unsafe static VideoFrame MatToVideoFrame(Mat mat)
         {
             using (Image<Bgr, byte> image = mat.ToImage<Bgr, byte>())
             {
                 VideoFrame frame = new VideoFrame(AVPixelFormat.AV_PIX_FMT_BGR24, image.Width, image.Height);
                 int stride = image.Width * image.NumberOfChannels;
-                for (int i = 0; i < frame.Height; i++)
+                for (int i = 0; i < frame.AVFrame.height; i++)
                 {
-                    Marshal.Copy(image.Bytes, i * stride, IntPtr.Add(frame.Data[0], frame.Linesize[0]), stride);
+                    Marshal.Copy(image.Bytes, i * stride, IntPtr.Add((IntPtr)frame.AVFrame.data[0], frame.AVFrame.linesize[0]), stride);
                 }
                 return frame;
             }
