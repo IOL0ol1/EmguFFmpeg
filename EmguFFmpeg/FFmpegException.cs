2f5ce0df2f23d41983814d6a65cf479afdfe3c1a
IOL0ol1 <1353839731@qq.com>
2019-11-11 19:01:55 +0800

    add exception message

--- EmguFFmpeg/FFmpegException.cs
+++ EmguFFmpeg/FFmpegException.cs
@@ -54,5 +54,6 @@ namespace EmguFFmpeg
         public const string InvalidAudioFrame = "invalid audio frame";
         public const string InvalidFrame = "invalid frame";
         public const string NotInitCodecContext = "not init codec context";
+        public const string TooManyChannels = "too many channels";
     }
 }
