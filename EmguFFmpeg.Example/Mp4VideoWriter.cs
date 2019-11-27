dcc7e9ef40b58db06bc61da89cc667245ed4c892
IOL0ol1 <1353839731@qq.com>
2019-11-28 01:49:26 +0800

    clear code

--- EmguFFmpeg.Example/Mp4VideoWriter.cs
+++ EmguFFmpeg.Example/Mp4VideoWriter.cs
@@ -154,16 +154,12 @@ namespace EmguFFmpeg.Example.Example
 
         #region IDisposable Support
 
-        private bool disposedValue = false; // 要检测冗余调用
+        private bool disposedValue = false;
 
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
             {
-                if (disposing)
-                {
-                    // TODO: 释放托管状态(托管对象)。
-                }
 
                 writer.FlushMuxer();
                 writer.Dispose();
@@ -172,19 +168,14 @@ namespace EmguFFmpeg.Example.Example
             }
         }
 
-        // TODO: 仅当以上 Dispose(bool disposing) 拥有用于释放未托管资源的代码时才替代终结器。
         ~Mp4VideoWriter()
         {
-            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
             Dispose(false);
         }
 
-        // 添加此代码以正确实现可处置模式。
         public void Dispose()
         {
-            // 请勿更改此代码。将清理代码放入以上 Dispose(bool disposing) 中。
             Dispose(true);
-            // TODO: 如果在以上内容中替代了终结器，则取消注释以下行。
             GC.SuppressFinalize(this);
         }
 
