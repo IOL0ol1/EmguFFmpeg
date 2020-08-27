a25e01d0bd53d8b0eef1db6a9df509d9a3cfbaff
IOL0ol1 <1353839731@qq.com>
2020-08-27 17:08:22 +0800

    add xmldoc

--- EmguFFmpeg.Bitmap/BitmapExtension.cs
+++ EmguFFmpeg.Bitmap/BitmapExtension.cs
@@ -12,7 +12,7 @@ namespace EmguFFmpeg
     /// </summary>
     public static class BitmapExtension
     {
-        private static Bitmap BgraToMat(VideoFrame frame)
+        private static Bitmap BgraToBitmap(VideoFrame frame)
         {
             int width = frame.Width;
             int height = frame.Height;
@@ -24,17 +24,27 @@ namespace EmguFFmpeg
             return bitmap;
         }
 
+        /// <summary>
+        /// convert to <see cref="PixelFormat.Format32bppArgb"/> or <see cref="PixelFormat.Format24bppRgb"/> bitmap
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <returns></returns>
         public static Bitmap ToBitmap(this VideoFrame frame)
         {
             if ((AVPixelFormat)frame.AVFrame.format == AVPixelFormat.AV_PIX_FMT_BGRA || (AVPixelFormat)frame.AVFrame.format == AVPixelFormat.AV_PIX_FMT_BGR24)
-                return BgraToMat(frame);
+                return BgraToBitmap(frame);
             using (VideoFrame dstFrame = new VideoFrame(frame.AVFrame.width, frame.AVFrame.height, AVPixelFormat.AV_PIX_FMT_BGRA))
             using (PixelConverter converter = new PixelConverter(dstFrame))
             {
-                return BgraToMat(converter.ConvertFrame(frame));
+                return BgraToBitmap(converter.ConvertFrame(frame));
             }
         }
 
+        /// <summary>
+        /// convert <see cref="PixelFormat.Format32bppArgb"/> or <see cref="PixelFormat.Format24bppRgb"/> bitmap to videoframe
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
         public static VideoFrame ToVideoFrame(this Bitmap bitmap)
         {
             if (bitmap.PixelFormat != PixelFormat.Format24bppRgb && bitmap.PixelFormat != PixelFormat.Format32bppArgb)
@@ -43,9 +53,6 @@ namespace EmguFFmpeg
             int height = bitmap.Height;
             VideoFrame frame = new VideoFrame(width, height, bitmap.PixelFormat == PixelFormat.Format24bppRgb ? AVPixelFormat.AV_PIX_FMT_BGR24 : AVPixelFormat.AV_PIX_FMT_BGRA);
             BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
-            // do not use frame.Data[0] = bitmapData.Scan0
-            // frame.Linesize[0] may not be equal bitmapData.Stride and both of them may not be equal to width * 3 (BGR24),
-            // because of memory alignment.
             var bytewidth = Math.Min(bitmapData.Stride, frame.Linesize[0]);
             FFmpegHelper.CopyPlane(bitmapData.Scan0, bitmapData.Stride, frame.Data[0], frame.Linesize[0], bytewidth, height);
             bitmap.UnlockBits(bitmapData);
