24e2aee02472bb158a2a66647c51cb9adb5aa2e8
IOL0ol1 <1353839731@qq.com>
2020-09-04 02:17:27 +0800

    TODO

---
+++ EmguFFmpeg/MediaRational.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FFmpeg.AutoGen;
+
+namespace EmguFFmpeg
+{
+    public class MediaRational
+    {
+        private AVRational rational;
+
+
+    }
+}
