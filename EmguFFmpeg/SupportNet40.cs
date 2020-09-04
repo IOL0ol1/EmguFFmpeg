56f31354a60ac3cd2e992464f95f3e5a3ff98fc6
IOL0ol1 <1353839731@qq.com>
2020-09-05 03:23:08 +0800

    delete useless interface

--- EmguFFmpeg/SupportNet40.cs
+++ EmguFFmpeg/SupportNet40.cs
@@ -16,20 +16,5 @@ namespace EmguFFmpeg
     {
         T this[int index] { get; }
     }
-
-
-    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
-    {
-
-        TValue this[TKey key] { get; }
-
-        IEnumerable<TKey> Keys { get; }
-
-        IEnumerable<TValue> Values { get; }
-
-        bool ContainsKey(TKey key);
-
-        bool TryGetValue(TKey key, out TValue value);
-    }
 #endif
 }
