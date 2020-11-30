e636792d1510697b8ffb4a6dbbb5b1ef8a572de2
IOL0ol1 <1353839731@qq.com>
2020-12-01 00:48:32 +0800

    TODO:Stream needs to be redesigned

--- src/MediaStream.cs
+++ src/MediaStream.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using FFmpeg.AutoGen;
 
 namespace EmguFFmpeg
 {
     public unsafe class MediaStream
     {
-        public MediaCodecContext Codec { get; set; }
 
         public static MediaStream FromNative(IntPtr pAVStream)
         {
             return FromNative((AVStream*)pAVStream);
         }
 
-        public  static MediaStream FromNative(AVStream* stream)
+        public static MediaStream FromNative(AVStream* stream)
         {
             return new MediaStream(stream);
         }
@@ -60,68 +60,7 @@ namespace EmguFFmpeg
         /// </summary>
         public int Index => pStream->index;
 
-        /// <summary>
-        /// <see cref="ffmpeg.av_codec_is_decoder(AVCodec*)"/>
-        /// </summary>
-        public bool HasDecoder => Codec == null ? false : Codec.IsDecoder;
-
-        /// <summary>
-        /// <see cref="ffmpeg.av_codec_is_encoder(AVCodec*)"/>
-        /// </summary>
-        public bool HasEncoder => Codec == null ? false : Codec.IsEncoder;
-
-        /// <summary>
-        /// Read a fram from <see cref="MediaPacket"/>
-        /// </summary>
-        /// <param name="packet"></param>
-        /// <returns></returns>
-        public IEnumerable<MediaFrame> ReadFrame(MediaPacket packet)
-        {
-            if (!HasDecoder)
-                yield break;
-            if (packet.StreamIndex != Index)
-                yield break;
-            foreach (var item in Codec.DecodePacket(packet))
-            {
-                yield return item;
-            }
-        }
-
-        /// <summary>
-        /// Write a fram by <see cref="Codec"/>.
-        /// <para><see cref="MediaEncoder.EncodeFrame(MediaFrame)"/></para>
-        /// <para><see cref="PacketRescaleTs(MediaPacket)"/></para>
-        /// </summary>
-        /// <param name="frame"></param>
-        /// <exception cref="FFmpegException"/>
-        /// <returns></returns>
-
-        public IEnumerable<MediaPacket> WriteFrame(MediaFrame frame)
-        {
-            if (!HasEncoder)
-                throw new FFmpegException(ffmpeg.AVERROR_ENCODER_NOT_FOUND);
-            foreach (var packet in GetCodecContext().EncodeFrame(frame))
-            {
-                PacketRescaleTs(packet);
-                packet.StreamIndex = Stream.index;
-                yield return packet;
-            }
-        }
-
-        private MediaCodecContext GetCodecContext()
-        {
-            return MediaCodecContext.FromNative(pStream->codec, false);
-        }
-
-        /// <summary>
-        /// convert packet pts from <see cref="AVCodecContext.time_base"/> to <see
-        /// cref="AVStream.time_base"/> and set <see cref="AVPacket.stream_index"/>
-        /// </summary>
-        /// <param name="packet"></param>
-        public void PacketRescaleTs(MediaPacket packet)
-        {
-            ffmpeg.av_packet_rescale_ts(packet, pStream->codec->time_base, Stream.time_base);
-        }
+  
 
         /// <summary>
         /// Convert to TimeSpan use <see cref="TimeBase"/>.
